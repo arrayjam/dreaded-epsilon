96353f9984018862a2b2073c1b2a1efbf635c0ea
Yuri Feldman <arrayjam@tbqh.net>
2013-09-06 01:28:48 +1000

    The general idea

--- visual.js
+++ visual.js
@@ -1,62 +1,228 @@
 var data = {
-  W: 0.5,
-  LW: 0.75,
-  LM: 0.15
+  left_w: 0.5,
+  left_h: 0.75,
+  right_h: 0.15
 };
 
-var w = data.LW * data.W;
-var m = data.LM * (1 - data.W);
+data.right_w = 1 - data.left_w;
+console.log(data);
 
-var r = w / (m + w);
-console.log(r);
+var color = d3.scale.category10();
+var scale = d3.scale.linear()
+  .domain([0, 1])
+  .rangeRound([0, 500]);
 
-var widths = [];
-[1, 2].map(function() {
-  widths.push(~~(Math.random() * 500));
-});
+var svg = d3.select("body").append("svg")
+  .append("g")
+  .attr("transform", "translate(" + [10, 10] + ")")
+  .datum([data]);
 
-var svg = d3.select("body").append("svg");
-var colors = d3.scale.category10();
-var offset = 0;
-
-var rect = svg.selectAll("rect")
-    .data(widths)
-  .enter().append("rect")
-    .attr({
-      x: function(d) { var x = offset; offset += d; return x; },
-      y: 0,
-      width: function(d) { return d; },
-      height: 50,
-      fill: function(d, i) { return colors(i); },
-      class: function(d, i) { return "rect-" + i; }
+/*  W  M
+ * -------
+ * |  |  | L
+ * -------
+ * |  |  | ~L
+ * ------
+ */
+function update() {
+  var rect, text, line, circle;
+  rect = svg.selectAll(".top.left")
+    .data(function(d) { return d; });
+  rect.enter().append("rect");
+  rect.attr({
+      class: "top left",
+      x: 0.5,
+      y: 0.5,
+      width: function(d) { return scale(d.left_w);  },
+      height: function(d) { return scale(d.left_h); },
+      fill: "none",
+      stroke: "black"
     });
 
-var width = d3.sum(widths);
-console.log(width);
+//  text = svg.selectAll("text.top.left")
+//    .data(function(d) { return d; });
+//  text.enter().append("text")
+//    .attr({
+//      class: "top left",
+//      x: function(d) { return scale(d.left_w) / 2; },
+//      y: function(d) { return scale(d.left_h) / 2; },
+//    })
+//    .style("text-anchor", "middle")
+//    .text("P(Long Hair | Woman)");
 
-var drag = d3.behavior.drag()
-  .on("drag", function() {
-    var x = d3.event.x;
-    if (x <= 0 || x >= width) return;
-    d3.select(".rect-0").attr("width", x);
-    d3.select(".rect-1").attr("x", x).attr("width", width - x);
-    line.attr("x1", x).attr("x2", x);
-    svg.style("cursor", "ew-resize");
-    //svg.selectAll
+  rect = svg.selectAll(".bottom.left")
+    .data(function(d) { return d; });
+  rect.enter().append("rect");
+  rect.attr({
+      class: "bottom left",
+      x: 0.5,
+      y: function(d) { return scale(d.left_h) + 0.5; },
+      width: function(d) { return scale(d.left_w); },
+      height: function(d) { return scale(1 - d.left_h); },
+      fill: "none",
+      stroke: "black"
+    });
+
+  rect = svg.selectAll(".top.right")
+    .data(function(d) { return d; });
+  rect.enter().append("rect");
+  rect.attr({
+      class: "top right",
+      x: function(d) { return scale(d.left_w) + 0.5; },
+      y: 0.5,
+      width: function(d) { return scale(d.right_w); },
+      height: function(d) { return scale(d.right_h); },
+      fill: "none",
+      stroke: "black"
+    });
+
+  rect = svg.selectAll(".bottom.right")
+    .data(function(d) { return d; });
+  rect.enter().append("rect");
+  rect.attr({
+      class: "bottom right",
+      x: function(d) { return scale(d.left_w) + 0.5; },
+      y: function(d) { return scale(d.right_h) + 0.5; },
+      width: function(d) { return scale(d.right_w); },
+      height: function(d) { return scale(1 - d.right_h); },
+      fill: "none",
+      stroke: "black"
+    });
+
+  line = svg.selectAll(".horizontal.resize")
+    .data(function(d) { return d; });
+  line.enter().append("line");
+  line.attr({
+      class: "horizontal resize",
+      x1: function(d) { return scale(d.left_w) + 0.5; },
+      x2: function(d) { return scale(d.left_w) + 0.5; },
+      y1: scale(0),
+      y2: scale(1),
+      stroke: "red"
     })
-  .on("dragend", function() {
-    svg.style("cursor", "auto");
-  });
+    .style("stroke-width", 7)
+    .style("opacity", 0)
+    .call(d3.behavior.drag()
+          .on("drag", function(d) {
+            if (d3.event.x <= scale(0) || d3.event.x >= scale(1))
+              return;
+            d.left_w = scale.invert(d3.event.x);
+            d.right_w = 1 - d.left_w;
+            svg.datum([data]);
+            update();
+            console.log(d);
+          })
+         );
 
-var line = svg.append("line")
-  .attr({
-    x1: widths[0],
-    x2: widths[0],
-    y1: 0,
-    y2: 50
-  })
-  .style({
-    stroke: "green",
-    "stroke-width": 10
-  })
-  .call(drag);
+  line = svg.selectAll(".vertical.resize.left")
+    .data(function(d) { return d; });
+  line.enter().append("line");
+  line.attr({
+      class: "vertical resize left",
+      x1: scale(0) + 0.5,
+      x2: function(d) { return scale(d.left_w) + 0.5; },
+      y1: function(d) { return scale(d.left_h) + 0.5; },
+      y2: function(d) { return scale(d.left_h) + 0.5; },
+      stroke: "red"
+    })
+    .style("stroke-width", 7)
+    .style("opacity", 0)
+    .call(d3.behavior.drag()
+          .on("drag", function(d) {
+            if (d3.event.y <= scale(0) || d3.event.y >= scale(1))
+              return;
+            d.left_h = scale.invert(d3.event.y);
+            svg.datum([data]);
+            update();
+            console.log(d);
+          })
+         );
+
+  line = svg.selectAll(".vertical.resize.right")
+    .data(function(d) { return d; });
+  line.enter().append("line");
+  line.attr({
+      class: "vertical resize right",
+      x1: function(d) { return scale(d.left_w) + 0.5; },
+      x2: scale(1) + 0.5,
+      y1: function(d) { return scale(d.right_h) + 0.5; },
+      y2: function(d) { return scale(d.right_h) + 0.5; },
+      stroke: "red"
+    })
+    .style("stroke-width", 7)
+    .style("opacity", 0)
+    .call(d3.behavior.drag()
+          .on("drag", function(d) {
+            if (d3.event.y <= scale(0) || d3.event.y >= scale(1))
+              return;
+            d.right_h = scale.invert(d3.event.y);
+            svg.datum([data]);
+            update();
+            console.log(d);
+          })
+         );
+
+//  circle = svg.selectAll(".left.mover")
+//    .data(function(d) { return d; });
+//  line.enter().append("circle");
+//  line.attr({
+//      class: "left mover",
+//      dx:,
+//      dy:,
+//      r: 5,
+//      y2: function(d) { return scale(d.right_h); },
+//      stroke: "black",
+//      strokeWidth: 4
+//    })
+//    .call(d3.behavior.drag()
+//          .on("drag", function(d) {
+//            if (d3.event.y <= scale(0) || d3.event.y >= scale(1))
+//              return;
+//            d.right_h = scale.invert(d3.event.y);
+//            svg.datum([data]);
+//            update();
+//            console.log(d);
+//          })
+//         );
+
+   var w = data.left_h * data.left_w;
+   var m = data.right_h * data.right_w;
+
+   var r = w / (m + w);
+   console.log(r);
+
+}
+//line = svg.selectAll(".vertical.resize")
+//  .data(function(d) { return d; })
+//  .enter().append("line")
+//  .attr({
+//    class: "vertical resize left",
+//    x1: 0,
+//    x2: function(d) { return scale(d.left_w); },
+//    y1: function(d) { return scale(d.left_h); },
+//    y2: function(d) { return scale(d.left_h); },
+//    stroke: "black",
+//    strokeWidth: 3
+//  })
+//  .call(d3.behavior.drag()
+//        .on("drag", function(d) {
+//          d.left_w = d3.event.y;
+//
+//
+//          //var y = d3.event.y;
+//          //if (y <= 0 || y >= scale(1)) return;
+//          //d3.selectAll(".top.left").attr("height", y);
+//          //d3.selectAll(".right").attr("x", x).attr("width", scale(1) - x);
+//          //d3.selectAll(".horizontal.resize").attr({
+//          //  x1: x,
+//          //  x2: x
+//          //});
+//        })
+//       );
+update();
+
+
+
+
+
+
