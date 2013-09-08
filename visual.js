00c3b59aa136bfa9c2039eb2ffd1c0afbfb319a3
Yuri Feldman <arrayjam@tbqh.net>
2013-09-09 00:13:24 +1000

    All the interactions

--- visual.js
+++ visual.js
@@ -6,22 +6,26 @@ var data = {
 
 console.log(data);
 
-var viz = BayesVisual().width(10).height(10);
-d3.selectAll("bv")
-  .data([data])
-  .call(viz);
+var viz = BayesVisual().side(300);
+console.log(viz.side());
+var bv = d3.selectAll("bv");
+
+var update = function() {
+  bv
+    .data([data])
+    .call(viz);
+};
+update();
 
 function BayesVisual() {
-  var top = 0,
-    left = 0,
-    width,
-    height,
-    scale = d3.scale.linear().domain([0, 1]).range([0, 300]),
+  var side = 300,
+    scale = d3.scale.linear().domain([0, 1]).range([0, side]),
     top_left, top_right, bottom_left, bottom_right,
     horizontal, left_vertical, right_vertical,
     left_move, right_move;
 
   var chart = function(selection) {
+    scale.range([0, side]);
     selection.each(function(data) {
       var div = d3.select(this);
 
@@ -54,6 +58,87 @@ function BayesVisual() {
           .attr({
             class: "outline bottom right"
           });
+
+        var horiz_drag = d3.behavior.drag()
+          .on("drag", function(d) {
+            var x = scale.invert(d3.event.x);
+            if (x < 0 || x > 1) return;
+            d.prior = x;
+            update();
+          });
+
+        horizontal = g.append("line")
+          .attr({
+            class: "horizontal resize",
+            y1: scale(0) + 0.5,
+            y2: scale(1) + 0.5
+          })
+          .call(horiz_drag);
+
+        var left_vert_drag = d3.behavior.drag()
+          .on("drag", function(d) {
+            var y = scale.invert(d3.event.y);
+            if (y < 0 || y > 1) return;
+            d.pos_likelihood = y;
+            update();
+          });
+
+        left_vertical = g.append("line")
+          .attr({
+            class: "vertical resize left",
+            x1: scale(0) + 0.5,
+          })
+          .call(left_vert_drag);
+
+        var right_vert_drag = d3.behavior.drag()
+          .on("drag", function(d) {
+            var y = scale.invert(d3.event.y);
+            if (y < 0 || y > 1) return;
+            d.neg_likelihood = y;
+            update();
+          });
+
+        right_vertical = g.append("line")
+          .attr({
+            class: "vertical resize right",
+            x2: scale(1) + 0.5,
+          })
+          .call(right_vert_drag);
+
+        var left_move_drag = d3.behavior.drag()
+          .on("drag", function(d) {
+            var x = scale.invert(d3.event.x);
+            var y = scale.invert(d3.event.y);
+            if (x < 0 || x > 1 || y < 0 || y > 1) return;
+            d.prior = x;
+            d.pos_likelihood = y;
+            update();
+          });
+
+        left_move = g.append("circle")
+          .attr({
+            class: "move left",
+            r: 5
+          })
+          .call(left_move_drag);
+
+        var right_move_drag = d3.behavior.drag()
+          .on("drag", function(d) {
+            var x = scale.invert(d3.event.x);
+            var y = scale.invert(d3.event.y);
+            if (x < 0 || x > 1 || y < 0 || y > 1) return;
+            d.prior = x;
+            d.neg_likelihood = y;
+            update();
+          });
+
+        right_move = g.append("circle")
+          .attr({
+            class: "move right",
+            r: 5
+          })
+          .call(right_move_drag);
+
       }
       top_left
         .attr({
@@ -82,31 +167,45 @@ function BayesVisual() {
           width: function(d) { return scale(1 - d.prior); },
           height: function(d) { return scale(1 - d.neg_likelihood); }
         });
+
+      horizontal
+        .attr({
+          x1: function(d) { return scale(d.prior) + 0.5; },
+          x2: function(d) { return scale(d.prior) + 0.5; },
+        });
+
+      left_vertical
+        .attr({
+          x2: function(d) { return scale(d.prior) + 0.5; },
+          y1: function(d) { return scale(d.pos_likelihood) + 0.5; },
+          y2: function(d) { return scale(d.pos_likelihood) + 0.5; },
+        });
+
+      right_vertical
+        .attr({
+          x1: function(d) { return scale(d.prior) + 0.5; },
+          y1: function(d) { return scale(d.neg_likelihood) + 0.5; },
+          y2: function(d) { return scale(d.neg_likelihood) + 0.5; },
+        });
+
+      left_move
+        .attr({
+          cx: function(d) { return scale(d.prior); },
+          cy: function(d) { return scale(d.pos_likelihood); }
+        });
+
+      right_move
+        .attr({
+          cx: function(d) { return scale(d.prior); },
+          cy: function(d) { return scale(d.neg_likelihood); }
+        });
       console.log("yup", data);
     });
   };
 
-  chart.top = function(_) {
-    if (!arguments.length) return top;
-    top = _;
-    return chart;
-  };
-
-  chart.left = function(_) {
-    if (!arguments.length) return left;
-    left = _;
-    return chart;
-  };
-
-  chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
-
-  chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
+  chart.side = function(_) {
+    if (!arguments.length) return side;
+    side = _;
     return chart;
   };
 
